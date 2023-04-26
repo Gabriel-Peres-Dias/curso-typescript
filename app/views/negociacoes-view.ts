3405cd36714867a769d38d1ce542ec374321f119
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-04-25 23:29:23 -0300

    Criando a view da tabela dos dados e da mensagem de sucesso através de templates. Reutilizamos código com o uso de POO por meio de herança e abstração para divir de forma coesa as responsabilidades das views.

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,30 @@
+import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
+
+export class NegociacoesView extends View<Negociacoes> {
+
+    template(model: Negociacoes): string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Quantidade</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+            ${ model.listar().map(negociacao => {
+                return `
+                <tr>
+                <td>${ new Intl.DateTimeFormat().format(negociacao.data) }</td>
+                <td>${ negociacao.quantidade}</td>
+                <td>${ negociacao.valor}</td>
+            </tr>
+                `;
+            }).join('')}  
+            </tbody>
+        </table>
+        `;
+    }
+}
---
+++ dist/js/views/negociacoes-view.js
@@ -0,0 +1,27 @@
+import { View } from "./view.js";
+export class NegociacoesView extends View {
+    template(model) {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Quantidade</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+            ${model.listar().map(negociacao => {
+            return `
+                <tr>
+                <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                <td>${negociacao.quantidade}</td>
+                <td>${negociacao.valor}</td>
+            </tr>
+                `;
+        }).join('')}  
+            </tbody>
+        </table>
+        `;
+    }
+}
